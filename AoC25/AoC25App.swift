93f3ac2b59f0f4c361a56de9ee7d091fd8bcfa1f
Mike Lewis <revmikelewis@gmail.com>
2025-12-02 11:46:38 -0600

    Initial Commit

---
+++ AoC25/AoC25App.swift
@@ -0,0 +1,17 @@
+//
+//  AoC25App.swift
+//  AoC25
+//
+//  Created by Mike Lewis on 12/2/25.
+//
+
+import SwiftUI
+
+@main
+struct AoC25App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
