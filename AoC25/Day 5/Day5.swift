45256d0370cf94f9e3aaab1ee2d9cdc87cc96632
Mike Lewis <revmikelewis@gmail.com>
2025-12-05 12:20:43 -0600

    Day 5 part 2 complete

--- AoC25/Day 5/Day5.swift
+++ AoC25/Day 5/Day5.swift
@@ -28,19 +28,43 @@ func Day5(file: String, part: Int) -> String {
             ranges.insert(idRange(min: min, max: max))
             
         } else {
-            print(line)
-            if ranges.contains(where: { $0.min...$0.max ~= Int(line)! }) {
-                tot += 1
+            if part == 1 {
+                
+                if ranges.contains(where: { $0.min...$0.max ~= Int(line)! }) {
+                    tot += 1
+                }
             }
         }
-        
     }
+    
+    if part == 2 {
+        let sortedRanges: [idRange] = ranges.sorted { $0.min < $1.min }
+        
+        var combinedRanges: [idRange] = [sortedRanges[0]]
+        for i in 1..<sortedRanges.count {
+//            print("\(i): Compare \(sortedRanges[i].min)-\(sortedRanges[i].max) to \(combinedRanges[combinedRanges.count-1].min)-\(combinedRanges[combinedRanges.count-1].max)")
+            if sortedRanges[i].min <= combinedRanges[combinedRanges.count-1].max {
+                if sortedRanges[i].max >= combinedRanges[combinedRanges.count-1].max {
+                    combinedRanges[combinedRanges.count-1].max = sortedRanges[i].max
+                }
+            } else {
+                combinedRanges.append(sortedRanges[i])
+            }
+            
+        }
+        
+        for cR in combinedRanges {
+            tot += cR.max - cR.min + 1
+            print("\(cR.min)...\(cR.max)")
+        }
+    }
+
     
     print(tot)
     return "\(tot)"
     
     struct idRange: Hashable {
-        let min: Int
-        let max: Int
+        var min: Int
+        var max: Int
     }
 }
